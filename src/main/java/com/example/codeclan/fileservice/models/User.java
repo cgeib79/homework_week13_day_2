776ff6f00e99ff1a392835d87d194b71ffe7c48b
Christian Geib <christian.geib79@gmail.com>
2018-10-10 07:50:57 +0100

    2nd commit

--- src/main/java/com/example/codeclan/fileservice/models/User.java
+++ src/main/java/com/example/codeclan/fileservice/models/User.java
@@ -1,4 +1,54 @@
 package com.example.codeclan.fileservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Folder> folders;
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
